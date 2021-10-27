1c541be0e239f3921c2ede6361924433076bc38a
baltasar <baltasarq@gmail.com>
2021-10-27 11:16:17 +0200

    Fix: coma en el mensaje

--- holamundo.cs
+++ holamundo.cs
@@ -3,7 +3,7 @@ namespace HolaMundo {
     class Saludo {
         public static void Main()
         {
-            System.Console.WriteLine( "Hola mundo !!" );
+            System.Console.WriteLine( "Hola, mundo !!" );
         }
     }
 }
