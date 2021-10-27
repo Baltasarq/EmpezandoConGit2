706fc31c211a6a6586e1476186f3b13f919febb4
baltasar <baltasarq@gmail.com>
2021-10-27 11:33:31 +0200

    Fix: el mensaje era demasiado largo

--- holamundo.cs
+++ holamundo.cs
@@ -3,7 +3,6 @@ namespace HolaMundo {
     class Saludo {
         static void Main()
         {
-            System.Console.WriteLine( "Mi primer proyect C#:" );
             System.Console.WriteLine( "Hola, mundo mundial !!!" );
         }
     }
