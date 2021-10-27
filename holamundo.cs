37b1c06414dab9125f72b9211fef9bb2da1ece2e
Baltasar García Perez-Schofield <baltasarq@gmail.com>
2021-10-27 11:13:30 +0200

    Update holamundo.cs
    Aclarado el mensaje

--- holamundo.cs
+++ holamundo.cs
@@ -3,7 +3,7 @@ namespace HolaMundo {
     class Saludo {
         public static void Main()
         {
-            System.Console.WriteLine( "Hola" );
+            System.Console.WriteLine( "Hola mundo !!" );
         }
     }
 }
